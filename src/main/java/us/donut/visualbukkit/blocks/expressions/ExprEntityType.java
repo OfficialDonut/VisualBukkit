64771c57e7ea6538a069db63e498692d1a12a527
OfficialDonut <donut42915@gmail.com>
2020-03-15 18:11:19 -0400

    EnumBlock

--- src/main/java/us/donut/visualbukkit/blocks/expressions/ExprEntityType.java
+++ src/main/java/us/donut/visualbukkit/blocks/expressions/ExprEntityType.java
@@ -1,36 +1,16 @@
 package us.donut.visualbukkit.blocks.expressions;
 
 import org.bukkit.entity.EntityType;
-import us.donut.visualbukkit.blocks.ExpressionBlock;
+import us.donut.visualbukkit.blocks.EnumBlock;
 import us.donut.visualbukkit.blocks.annotations.Category;
 import us.donut.visualbukkit.blocks.annotations.Description;
-import us.donut.visualbukkit.blocks.syntax.ChoiceParameter;
-import us.donut.visualbukkit.blocks.syntax.SyntaxNode;
 
 @Category("Entity")
 @Description({"An entity type", "Returns: entity type"})
-public class ExprEntityType extends ExpressionBlock {
-
-    private static String[] entityTypes = new String[EntityType.values().length];
-
-    static {
-        for (int i = 0; i < EntityType.values().length; i++) {
-            entityTypes[i] = EntityType.values()[i].name();
-        }
-    }
+public class ExprEntityType extends EnumBlock {
 
     @Override
-    protected SyntaxNode init() {
-        return new SyntaxNode(new ChoiceParameter(entityTypes));
-    }
-
-    @Override
-    public String toJava() {
-        return EntityType.class.getCanonicalName() + "." + arg(0);
-    }
-
-    @Override
-    public Class<?> getReturnType() {
+    public Class<? extends Enum<?>> getEnum() {
         return EntityType.class;
     }
 }
--- src/main/java/us/donut/visualbukkit/blocks/expressions/ExprInventoryType.java
+++ src/main/java/us/donut/visualbukkit/blocks/expressions/ExprInventoryType.java
@@ -1,37 +1,16 @@
 package us.donut.visualbukkit.blocks.expressions;
 
-import org.bukkit.entity.EntityType;
 import org.bukkit.event.inventory.InventoryType;
-import us.donut.visualbukkit.blocks.ExpressionBlock;
+import us.donut.visualbukkit.blocks.EnumBlock;
 import us.donut.visualbukkit.blocks.annotations.Category;
 import us.donut.visualbukkit.blocks.annotations.Description;
-import us.donut.visualbukkit.blocks.syntax.ChoiceParameter;
-import us.donut.visualbukkit.blocks.syntax.SyntaxNode;
 
 @Category("Inventory")
 @Description({"An inventory type", "Returns: inventory type"})
-public class ExprInventoryType extends ExpressionBlock {
-
-    private static String[] inventoryTypes = new String[EntityType.values().length];
-
-    static {
-        for (int i = 0; i < InventoryType.values().length; i++) {
-            inventoryTypes[i] = InventoryType.values()[i].name();
-        }
-    }
+public class ExprInventoryType extends EnumBlock {
 
     @Override
-    protected SyntaxNode init() {
-        return new SyntaxNode(new ChoiceParameter(inventoryTypes));
-    }
-
-    @Override
-    public String toJava() {
-        return InventoryType.class.getCanonicalName() + "." + arg(0);
-    }
-
-    @Override
-    public Class<?> getReturnType() {
+    public Class<? extends Enum<?>> getEnum() {
         return InventoryType.class;
     }
 }
