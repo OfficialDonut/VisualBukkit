b67ba341114e83f61b56c345832c0edf62162ee4
OfficialDonut <donut42915@gmail.com>
2020-06-06 13:03:17 -0400

    Add event annotation

--- src/main/java/us/donut/visualbukkit/blocks/expressions/ExprDamagedItem.java
+++ src/main/java/us/donut/visualbukkit/blocks/expressions/ExprDamagedItem.java
@@ -4,10 +4,12 @@ import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.inventory.ItemStack;
 import us.donut.visualbukkit.blocks.ExpressionBlock;
 import us.donut.visualbukkit.blocks.annotations.Description;
+import us.donut.visualbukkit.blocks.annotations.Event;
 import us.donut.visualbukkit.blocks.syntax.SyntaxNode;
 import us.donut.visualbukkit.editor.EventPane;
 
 @Description({"The damaged item in a PlayerItemDamageEvent", "Returns: item stack"})
+@Event(PlayerItemDamageEvent.class)
 public class ExprDamagedItem extends ExpressionBlock<ItemStack> {
 
     @Override
