c90c4644f225291ac12dc6b81fa5276d9a786663
OfficialDonut <donut42915@gmail.com>
2020-09-07 15:34:25 -0400

    Add event item for PlayerItemConsumeEvent

--- src/main/java/us/donut/visualbukkit/blocks/expressions/ExprEventItem.java
+++ src/main/java/us/donut/visualbukkit/blocks/expressions/ExprEventItem.java
@@ -1,12 +1,13 @@
 package us.donut.visualbukkit.blocks.expressions;
 
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
 import us.donut.visualbukkit.blocks.ExpressionBlock;
 import us.donut.visualbukkit.blocks.annotations.Description;
 import us.donut.visualbukkit.blocks.syntax.Syntax;
 
-@Description({"The item in a PlayerInteractEvent", "Returns: item stack"})
+@Description({"The item in a PlayerInteractEvent or PlayerItemConsumeEvent", "Returns: item stack"})
 public class ExprEventItem extends ExpressionBlock<ItemStack> {
 
     @Override
@@ -17,7 +18,7 @@ public class ExprEventItem extends ExpressionBlock<ItemStack> {
     @Override
     public void update() {
         super.update();
-        validateEvent(PlayerInteractEvent.class);
+        validateEvent(PlayerInteractEvent.class, PlayerItemConsumeEvent.class);
     }
 
     @Override
