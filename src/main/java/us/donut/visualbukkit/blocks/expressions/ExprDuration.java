05594287f078d94ad88711261eaa5e53b554d96b
OfficialDonut <donut42915@gmail.com>
2020-09-07 14:40:29 -0400

    Fix fractional durations

--- src/main/java/us/donut/visualbukkit/blocks/expressions/ExprDuration.java
+++ src/main/java/us/donut/visualbukkit/blocks/expressions/ExprDuration.java
@@ -1,6 +1,5 @@
 package us.donut.visualbukkit.blocks.expressions;
 
-import org.apache.commons.lang.WordUtils;
 import us.donut.visualbukkit.blocks.ExpressionBlock;
 import us.donut.visualbukkit.blocks.annotations.Description;
 import us.donut.visualbukkit.blocks.syntax.ChoiceParameter;
@@ -13,14 +12,29 @@ public class ExprDuration extends ExpressionBlock<Duration> {
 
     @Override
     protected Syntax init() {
-        return new Syntax(long.class, new ChoiceParameter("ticks", "seconds", "minutes", "hours", "days"));
+        return new Syntax(double.class, new ChoiceParameter("ticks", "seconds", "minutes", "hours", "days"));
     }
 
     @Override
     public String toJava() {
-        String unit = arg(1);
-        return unit.equals("ticks") ?
-                "java.time.Duration.ofMillis(" + arg(0) + "*50)" :
-                "java.time.Duration.of" + WordUtils.capitalize(unit) + "(" + arg(0) + ")";
+        String time = "(" + arg(0) + ")";
+        switch (arg(1)) {
+            case "ticks":
+                time = time + "*50";
+                break;
+            case "seconds":
+                time = time + "*1000";
+                break;
+            case "minutes":
+                time = time + "*60*1000";
+                break;
+            case "hours":
+                time = time + "*60*60*1000";
+                break;
+            case "days":
+                time = time + "*24*60*60*1000";
+                break;
+        }
+        return "java.time.Duration.ofMillis((long) (" + time + "))";
     }
 }
