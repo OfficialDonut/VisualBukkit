218addcb3f1a942fe4d2513afd07590c9f40bf4a
Dablakbandit <ashle.thew@gmail.com>
2021-01-11 01:36:35 +1300

    Create JsonUrlReader.java

---
+++ src/main/java/com/gmail/visualbukkit/util/JsonUrlReader.java
@@ -0,0 +1,32 @@
+package com.gmail.visualbukkit.util;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+public class JsonUrlReader {
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
+}
