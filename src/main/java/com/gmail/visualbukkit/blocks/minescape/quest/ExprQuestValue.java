61efd43eaf070a39851fceb7aa98da912d4c8560
Dablakbandit <ashle.thew@gmail.com>
2021-01-12 08:14:32 +1300

    Quest expression
    Quest expression
    Remove if statements

--- src/main/java/com/gmail/visualbukkit/blocks/minescape/quest/ExprQuestValue.java
+++ src/main/java/com/gmail/visualbukkit/blocks/minescape/quest/ExprQuestValue.java
@@ -1,26 +1,27 @@
 package com.gmail.visualbukkit.blocks.minescape.quest;
 
-import com.gmail.visualbukkit.blocks.ParentBlock;
+import com.gmail.visualbukkit.blocks.ExpressionBlock;
+import com.gmail.visualbukkit.blocks.annotations.Description;
+import com.gmail.visualbukkit.blocks.components.BinaryChoiceParameter;
 import com.gmail.visualbukkit.blocks.components.ChoiceParameter;
-import com.gmail.visualbukkit.blocks.minescape.quest.QuestChoiceParameter;
+import com.gmail.visualbukkit.blocks.minescape.MineScape;
 import javafx.collections.FXCollections;
 
 import java.util.Collections;
 
-public abstract class StatQuestStatement extends ParentBlock {
+@Description("A boolean (true or false)")
+public class ExprQuestValue extends ExpressionBlock<MineScape> {
 
     private ChoiceParameter questChoice = new QuestChoiceParameter();
     private ChoiceParameter checkChoice = new ChoiceParameter("<", "<=", "==", "=>", ">");
     private ChoiceParameter valueChoice = new ChoiceParameter(Collections.emptyList(), false);
 
-    public StatQuestStatement() {
+    public ExprQuestValue() {
         questChoice.valueProperty().addListener((e) -> setQuestChoiceItems());
         valueChoice.valueProperty().addListener((e) -> update());
         checkChoice.setMaxWidth(20);
-        init(getStatement() + " ", questChoice, " ", checkChoice, " ", valueChoice);
+        init(questChoice, " ", checkChoice, " ", valueChoice);
     }
-
-    public abstract String getStatement();
 
     private void setQuestChoiceItems(){
         valueChoice.setItems(FXCollections.observableArrayList(QuestChoiceParameter.getChoiceParameter(questChoice.getValue())));
@@ -28,19 +29,7 @@ public abstract class StatQuestStatement extends ParentBlock {
     }
 
     @Override
-    public void update() {
-        super.update();
-        if(valueChoice.getValue() ==null){
-            super.setInvalid("Missing value.");
-        }else if(questChoice.getValue() != null && valueChoice.getValue() != null){
-            if(!QuestChoiceParameter.getChoiceParameter(questChoice.getValue()).contains(valueChoice.getValue())){
-                super.setInvalid("Quests do not match");
-            }
-        }
-    }
-
-    @Override
     public String toJava() {
-        return "";
+        return arg(0);
     }
 }
