cf1b5c5c9fd2bd352dee0fdcc6bd33ae4df2e6e1
OfficialDonut <donut42915@gmail.com>
2021-04-14 18:14:55 -0400

    Add theme reload

--- VB-Application/src/main/java/com/gmail/visualbukkit/SettingsManager.java
+++ VB-Application/src/main/java/com/gmail/visualbukkit/SettingsManager.java
@@ -23,7 +23,8 @@ public class SettingsManager extends Menu {
 
     private static final SettingsManager instance = new SettingsManager();
 
-    private final Map<String, String> themes = new TreeMap<>();
+    private Menu themeMenu = new Menu("Theme");
+    private Map<String, String> themes = new TreeMap<>();
     private final StringProperty theme = new SimpleStringProperty();
     private final IntegerProperty fontSize = new SimpleIntegerProperty();
     private final BooleanProperty sounds = new SimpleBooleanProperty();
@@ -34,9 +35,7 @@ public class SettingsManager extends Menu {
         super("Settings");
     }
 
-    @SuppressWarnings("UnstableApiUsage")
     public void loadSettings(DataFile dataFile) throws IOException {
-        Menu themeMenu = new Menu("Theme");
         Menu fontSizeMenu = new Menu("Font Size");
         Menu soundsMenu = new Menu("Sounds");
         Menu autosaveTimeMenu = new Menu("Auto-save");
@@ -44,38 +43,10 @@ public class SettingsManager extends Menu {
         getItems().setAll(themeMenu, fontSizeMenu, soundsMenu, autosaveTimeMenu, langMenu);
 
         JSONObject json = dataFile.getJson();
-        String selectedTheme = json.optString("settings.theme", "Dark");
         String selectedLang = json.optString("settings.lang", "System Default");
         int selectedFontSize = json.optInt("settings.font-size", 14);
         int selectedAutosaveTime = json.optInt("settings.autosave", -1);
-
-        themes.put("Dark", "/themes/Dark.css");
-        themes.put("Light", "/themes/Light.css");
-        Path themeDir = VisualBukkitApp.getInstance().getDataDir().resolve("Themes");
-        if (Files.exists(themeDir)) {
-            try (DirectoryStream<Path> pathStream = Files.newDirectoryStream(themeDir, p -> p.toString().endsWith(".css"))) {
-                for (Path path : pathStream) {
-                    themes.put(MoreFiles.getNameWithoutExtension(path), path.toUri().toString());
-                }
-            }
-        }
-
-        Menu selectThemeMenu = new Menu("Select");
-        MenuItem openThemeDirItem = new MenuItem("Open Folder");
-        openThemeDirItem.setOnAction(e -> VisualBukkitApp.getInstance().openDirectory(themeDir));
-        themeMenu.getItems().addAll(selectThemeMenu, openThemeDirItem);
-
-        ToggleGroup themeGroup = new ToggleGroup();
-        for (String s : themes.keySet()) {
-            RadioMenuItem themeItem = new RadioMenuItem(s);
-            themeItem.setOnAction(e -> theme.set(s));
-            themeGroup.getToggles().add(themeItem);
-            selectThemeMenu.getItems().add(themeItem);
-            if (getTheme() == null || s.equals(selectedTheme)) {
-                theme.set(s);
-                themeItem.setSelected(true);
-            }
-        }
+        reloadThemes(json.optString("settings.theme", "Dark"));
 
         ToggleGroup soundsGroup = new ToggleGroup();
         RadioMenuItem enableSoundsItem = new RadioMenuItem("Enable");
@@ -132,6 +103,66 @@ public class SettingsManager extends Menu {
         }
     }
 
+    @SuppressWarnings("UnstableApiUsage")
+    private void reloadThemes(String selectedTheme) throws IOException {
+        theme.set(null);
+        themes.clear();
+        themeMenu.getItems().clear();
+
+        themes.put("Dark", "/themes/Dark.css");
+        themes.put("Light", "/themes/Light.css");
+
+        Path themeDir = VisualBukkitApp.getInstance().getDataDir().resolve("Themes");
+        if (Files.exists(themeDir)) {
+            try (DirectoryStream<Path> pathStream = Files.newDirectoryStream(themeDir, p -> p.toString().endsWith(".css"))) {
+                for (Path path : pathStream) {
+                    themes.put(MoreFiles.getNameWithoutExtension(path), path.toUri().toString());
+                }
+            }
+        }
+
+        Menu selectThemeMenu = new Menu("Select");
+        MenuItem openThemeDirItem = new MenuItem("Open Folder");
+        MenuItem reloadThemesItem = new MenuItem("Reload");
+        openThemeDirItem.setOnAction(e -> VisualBukkitApp.getInstance().openDirectory(themeDir));
+        reloadThemesItem.setOnAction(e -> {
+            try {
+                reloadThemes(getTheme());
+            } catch (Exception ex) {
+                NotificationManager.displayException("Failed to reload themes", ex);
+            }
+        });
+        themeMenu.getItems().addAll(selectThemeMenu, openThemeDirItem, reloadThemesItem);
+
+        ToggleGroup themeGroup = new ToggleGroup();
+        for (String s : themes.keySet()) {
+            RadioMenuItem themeItem = new RadioMenuItem(s);
+            themeItem.setOnAction(e -> theme.set(s));
+            themeGroup.getToggles().add(themeItem);
+            selectThemeMenu.getItems().add(themeItem);
+            if (getTheme() == null || s.equals(selectedTheme)) {
+                theme.set(s);
+                themeItem.setSelected(true);
+            }
+        }
+    }
+
+    public void bindStyle(Parent parent) {
+        parent.styleProperty().bind(Bindings.concat("-fx-font-size: ", fontSize));
+        theme.addListener((o, oldValue, newTheme) -> {
+            parent.getStylesheets().clear();
+            parent.getStylesheets().add("/themes/Base.css");
+            if (newTheme != null && themes.containsKey(newTheme)) {
+                parent.getStylesheets().add(themes.get(newTheme));
+            }
+        });
+        parent.getStylesheets().clear();
+        parent.getStylesheets().add("/themes/Base.css");
+        if (getTheme() != null && themes.containsKey(getTheme())) {
+            parent.getStylesheets().add(themes.get(getTheme()));
+        }
+    }
+
     public void saveSettings(DataFile dataFile) {
         JSONObject data = dataFile.getJson();
         data.put("settings.theme", getTheme());
@@ -139,12 +170,6 @@ public class SettingsManager extends Menu {
         data.put("settings.sounds", getSounds());
         data.put("settings.autosave", getAutosaveTime());
         data.put("settings.lang", getLang());
-    }
-
-    public void bindStyle(Parent parent) {
-        theme.addListener((o, oldValue, newTheme) -> parent.getStylesheets().setAll("/themes/Base.css", themes.get(newTheme)));
-        parent.getStylesheets().setAll("/themes/Base.css", themes.get(getTheme()));
-        parent.styleProperty().bind(Bindings.concat("-fx-font-size: ", fontSize));
     }
 
     public static SettingsManager getInstance() {
