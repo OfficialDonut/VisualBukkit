fbe4d48de2c28a0e729f7cc3bfb0b0f97f948216
OfficialDonut <donut42915@gmail.com>
2021-04-21 18:20:32 -0400

    Add simple global variables

---
+++ VB-Application/src/main/java/com/gmail/visualbukkit/blocks/definitions/ExprSimpleGlobalVariable.java
@@ -0,0 +1,45 @@
+package com.gmail.visualbukkit.blocks.definitions;
+
+import com.gmail.visualbukkit.blocks.ClassInfo;
+import com.gmail.visualbukkit.blocks.Expression;
+import com.gmail.visualbukkit.blocks.parameters.InputParameter;
+import com.gmail.visualbukkit.plugin.BuildContext;
+
+public class ExprSimpleGlobalVariable extends Expression {
+
+    public ExprSimpleGlobalVariable() {
+        super("expr-simple-global-variable", ClassInfo.OBJECT);
+    }
+
+    @Override
+    public Block createBlock() {
+        Block block = new Block(this) {
+            @Override
+            public void prepareBuild(BuildContext buildContext) {
+                super.prepareBuild(buildContext);
+                String varName = getVariable(arg(0));
+                if (!buildContext.getMainClass().hasField(varName)) {
+                    buildContext.getMainClass().addField("public static Object " + varName + ";");
+                }
+            }
+
+            @Override
+            public String toJava() {
+                return getVariable(arg(0));
+            }
+        };
+
+        InputParameter inputParameter = new InputParameter();
+        inputParameter.getStyleClass().add("simple-global-variable");
+        block.getHeaderBox().getChildren().clear();
+        block.addToHeader(inputParameter);
+        block.getHeaderBox().getStyleClass().clear();
+        block.getSyntaxBox().getStyleClass().clear();
+
+        return block;
+    }
+
+    protected static String getVariable(String string) {
+        return ExprSimpleLocalVariable.getVariable("global" + string);
+    }
+}
---
+++ VB-Application/src/main/java/com/gmail/visualbukkit/blocks/definitions/StatSetSimpleGlobalVariable.java
@@ -0,0 +1,36 @@
+package com.gmail.visualbukkit.blocks.definitions;
+
+import com.gmail.visualbukkit.blocks.ClassInfo;
+import com.gmail.visualbukkit.blocks.Statement;
+import com.gmail.visualbukkit.blocks.parameters.ExpressionParameter;
+import com.gmail.visualbukkit.blocks.parameters.InputParameter;
+import com.gmail.visualbukkit.plugin.BuildContext;
+
+public class StatSetSimpleGlobalVariable extends Statement {
+
+    public StatSetSimpleGlobalVariable() {
+        super("stat-set-simple-global-variable");
+    }
+
+    @Override
+    public Block createBlock() {
+        InputParameter inputParameter = new InputParameter();
+        inputParameter.getStyleClass().add("simple-global-variable");
+
+        return new Block(this, inputParameter, new ExpressionParameter(ClassInfo.OBJECT)) {
+            @Override
+            public void prepareBuild(BuildContext buildContext) {
+                super.prepareBuild(buildContext);
+                String varName = ExprSimpleGlobalVariable.getVariable(arg(0));
+                if (!buildContext.getMainClass().hasField(varName)) {
+                    buildContext.getMainClass().addField("public static Object " + varName + ";");
+                }
+            }
+
+            @Override
+            public String toJava() {
+                return ExprSimpleGlobalVariable.getVariable(arg(0)) + " = " + arg(1) + ";";
+            }
+        };
+    }
+}
