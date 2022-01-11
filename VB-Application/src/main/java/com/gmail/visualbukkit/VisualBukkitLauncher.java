214068e2521da41a6554b9554bd6ba0d36ab0622
eight_y_88 <jadec.eight@gmail.com>
2022-01-11 11:47:24 +0900

    [Update] Set false "prism.lcdtext" for smooth font rendering

--- VB-Application/src/main/java/com/gmail/visualbukkit/VisualBukkitLauncher.java
+++ VB-Application/src/main/java/com/gmail/visualbukkit/VisualBukkitLauncher.java
@@ -10,6 +10,7 @@ public class VisualBukkitLauncher {
     public static void main(String[] args) {
         try {
             System.setProperty("javafx.preloader", "com.gmail.visualbukkit.VisualBukkitPreloader");
+            System.setProperty("prism.lcdtext", "false");
             Application.launch(VisualBukkitApp.class);
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, Throwables.getStackTraceAsString(e), "Failed to launch Visual Bukkit", JOptionPane.ERROR_MESSAGE);
