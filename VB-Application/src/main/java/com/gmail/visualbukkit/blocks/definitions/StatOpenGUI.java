6c56133b83161684f7a14a30f9c0d64bf14f090e
Sal4iDev <96550478+Sal4iDev@users.noreply.github.com>
2022-01-20 17:10:57 +0300

    Some Changes
    - Added `Deep` in `[Config] Config Keys`
    - Changed ExpressionParameter `GUI` in `[Player] Open GUI` to `InputParameter` with label `GUI ID`

--- VB-Application/src/main/java/com/gmail/visualbukkit/blocks/definitions/StatOpenGUI.java
+++ VB-Application/src/main/java/com/gmail/visualbukkit/blocks/definitions/StatOpenGUI.java
@@ -3,6 +3,7 @@ package com.gmail.visualbukkit.blocks.definitions;
 import com.gmail.visualbukkit.blocks.ClassInfo;
 import com.gmail.visualbukkit.blocks.Statement;
 import com.gmail.visualbukkit.blocks.parameters.ExpressionParameter;
+import com.gmail.visualbukkit.blocks.parameters.InputParameter;
 import com.gmail.visualbukkit.project.BuildContext;
 import com.gmail.visualbukkit.project.PluginModule;
 
@@ -14,7 +15,10 @@ public class StatOpenGUI extends Statement {
 
     @Override
     public Block createBlock() {
-        return new Block(this, new ExpressionParameter("GUI", ClassInfo.STRING), new ExpressionParameter("Player", ClassInfo.of("org.bukkit.entity.Player"))) {
+        return new Block(this,
+                new InputParameter("GUI ID"),
+                new ExpressionParameter("Player", ClassInfo.of("org.bukkit.entity.Player"))
+        ) {
             @Override
             public void prepareBuild(BuildContext buildContext) {
                 super.prepareBuild(buildContext);
